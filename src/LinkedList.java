514d14d616b2896300efcd40d3e006bfc1b4c851
GoeschWho <mlgoesch11@gmail.com>
2017-10-17 13:56:11 -0500

    Create My Classes
    I added the BinarySearchTree and Linked List classes which implement the provided interfaces.  I class was also created with a main method for testing.

---
+++ src/LinkedList.java
@@ -0,0 +1,70 @@
+
+public class LinkedList<T> implements Sequence<T> {
+
+	@Override
+	public void add(T obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void add(int idx, T obj) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public T get(int idx) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(T obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int indexOf(T obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T remove(int idx) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(T obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public T[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
